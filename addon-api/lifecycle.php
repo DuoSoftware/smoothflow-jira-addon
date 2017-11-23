5170a348b6b6108d02ec80330fc1e272df45f92b
KalanaDemel2 <kalana@duosoftware.com>
2017-11-23 12:05:30 +0530

    Added webhook forward method

--- addon-api/lifecycle.php
+++ addon-api/lifecycle.php
@@ -180,7 +180,7 @@ class client
 
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, SVC_NGINXPROXYMAKER_HOST.'/test'.$_SERVER['QUERY_STRING']);
+        curl_setopt($ch, CURLOPT_URL, 'https://ngincproxymaker.plus.smoothflow.io/test'.$_SERVER['QUERY_STRING']);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('body' => $post)));
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
