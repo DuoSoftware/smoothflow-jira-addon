8401ccb35d2cc84f204e6c53f841a46246de7b1a
shehantissera <shehan@duosoftware.com>
2017-11-24 16:11:36 +0530

    Added screenshot

--- core/controllers/screenshot.controller.js
+++ core/controllers/screenshot.controller.js
@@ -1,16 +1,23 @@
 app.controller('ScreenshotController', ['$scope', '$rootScope', '$http', '$auth', function ($scope, $rootScope, $http, $auth) {
 
-    var self = this;
-    self.imageApi;
-    self.sendImage = sendImage;
-    self.isUrlOpen = false;
-    function sendImage() {
-        if (self.imageApi) {
-            self.imageApi.toPng(function (dataUrl) {
+    $scope.imageApi;
+    $scope.isUrlOpen = false;
+    $scope.sendImage = function() {
+        if ($scope.imageApi) {
+            $scope.imageApi.toPng(function (dataUrl) {
                 console.log(dataUrl);
                 alert('Please open console and print dataUrl then you can send dataUrl to your backend api do more feature like send mail.');
             });
         }
+    }
+
+    $scope.displayHideScreenshot = function(){
+        $scope.isUrlOpen = !$scope.isUrlOpen;
+        // if($scope.isUrlOpen){
+        //     //document.getElementById("screenFilter").style.zIndex = "2000000002";
+        // }else{
+        //     document.getElementById("screenFilter").style.zIndex = "-1";
+        // }
     }
 
 }]);
