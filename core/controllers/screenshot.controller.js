4aaa78a06842af7905cfc861ae9c58245b18ea91
lakmimi15 <lakmini@duosoftware.com>
2017-11-29 16:45:33 +0530

    WIP

--- core/controllers/screenshot.controller.js
+++ core/controllers/screenshot.controller.js
@@ -1,17 +1,22 @@
-app.controller('ScreenshotController', ['$scope', '$rootScope', '$http', '$auth', function ($scope, $rootScope, $http, $auth) {
+app.controller('ScreenshotController', ['$scope', '$rootScope', '$http', '$auth', '$uploader', '$storage', function ($scope, $rootScope, $http, $auth, uploader, storage) {
 
     $scope.imageApi;
     $scope.isUrlOpen = false;
-    $scope.sendImage = function() {
+    $scope.sendImage = function () {
         if ($scope.imageApi) {
             $scope.imageApi.toPng(function (dataUrl) {
+                //var aa = Math.random()
+                var randomnumber = Math.floor(Math.random() * 21) - 10;
                 console.log(dataUrl);
-                $rootScope.changeLocation('feedback');
+                $scope.name = "Screenshot" + randomnumber + ".png";
+                uploader.uploadMedia(window.location.hostname, dataUrl, $scope.name);
+
+
             });
         }
     }
 
-    $scope.displayHideScreenshot = function(){
+    $scope.displayHideScreenshot = function () {
         $scope.isUrlOpen = !$scope.isUrlOpen;
         // if($scope.isUrlOpen){
         //     //document.getElementById("screenFilter").style.zIndex = "2000000002";
@@ -19,5 +24,20 @@ app.controller('ScreenshotController', ['$scope', '$rootScope', '$http', '$auth'
         //     document.getElementById("screenFilter").style.zIndex = "-1";
         // }
     }
+
+    $rootScope.$on('uploader_success', function (event, data) {
+        debugger;
+        console.log('uploader_success recived:', data);
+        $rootScope.imageUrl = storage.getMediaUrl(window.location.hostname, $scope.name);
+        console.log('Image Url', $rootScope.imageUrl);        //set image name
+
+        $rootScope.changeLocation('feedback');
+    });
+
+    $rootScope.$on('uploader_fail', function (event, data) {
+        debugger;
+        console.log('uploader_fail recived:', data);
+
+    });
 
 }]);
