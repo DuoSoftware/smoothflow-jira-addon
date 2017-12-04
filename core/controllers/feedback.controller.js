561527c00c7ee2cfcf3e8b6ff2fa29b29a0364e7
lakmimi15 <lakmini@duosoftware.com>
2017-12-04 18:18:46 +0530

    WIP

--- core/controllers/feedback.controller.js
+++ core/controllers/feedback.controller.js
@@ -1,23 +1,43 @@
 app.controller('FeedbackController', ['$scope', '$http', '$v6urls', '$rootScope', 'dataHandler', '$uploader', '$storage', function ($scope, $http, $v6urls, $rootScope, dataHandler, uploader, storage) {
-    $scope.IsNew = true;
+    $scope.IsNew = false;
     $scope.Tasktypes = ['Complain', 'Bug', 'Improvement'];
     $scope.IsImageShow = false;
+    $scope.IsFirst = true;
+    $scope.IsSatus = false;
+
     /** Test Data */
     // var response = { "Status": true, "Message": "Successfully created the Issue!", "Data": { "IssueID": "52981", "IssueKey": "DSF-1079", "IssueUrl": "https://duosoftware.atlassian.net/rest/api/2/issue/52981" } }
     // $scope.ticketDetails = response.Data;
     // $scope.Message = response.Message;
     // $scope.Status = response.Status;
-
+    var res = "";
+    $scope.tasklist = "";
+    $scope.name="New";
 
 
     $scope.CreateNewTicket = function (IsNew) {
+        debugger;
+        if (IsNew) {
+            $scope.IsNew = false;
+            $scope.IsFirst = true;
+            $scope.IsSatus = false;
+            $scope.name="New";
+        } else {
+            $scope.IsNew = true;
+            $scope.IsFirst = false;
+            $scope.IsSatus = false;
+            $scope.name="Details";
+        }
+
         $scope.ticket = [];
+        $rootScope.imageUrl = "";
     };
     //Create Ticket
     $scope.uploadImage = function () {
         debugger;
         angular.element(document.querySelector('#file')).on('change', function () {
             $scope.name = this.files[0].name;
+            $rootScope.ShowBusyContainer("Uploading Image");
             uploader.V1uploadMedia(window.location.hostname, this.files[0], $scope.name);
         })
 
@@ -25,6 +45,7 @@ app.controller('FeedbackController', ['$scope', '$http', '$v6urls', '$rootScope'
     };
     $rootScope.$on('uploader_success', function (event, data) {
         debugger;
+        $rootScope.HideBusyContainer();
         console.log('uploader_success recived:', data);
         $rootScope.imageUrl = storage.getMediaUrl(window.location.hostname, $scope.name);
         $scope.IsImageShow = true;
@@ -33,10 +54,13 @@ app.controller('FeedbackController', ['$scope', '$http', '$v6urls', '$rootScope'
 
     $rootScope.$on('uploader_fail', function (event, data) {
         debugger;
+        $rootScope.HideBusyContainer();
+        $rootScope.DisplayMessage("Uploader Fail", 'error', "Please Contact Administrator");
         console.log('uploader_fail recived:', data);
 
     });
     $scope.CreateTicket = function () {
+        $rootScope.ShowBusyContainer("Processing");
         var ticket =
             {
                 "channel": "api",
@@ -59,14 +83,42 @@ app.controller('FeedbackController', ['$scope', '$http', '$v6urls', '$rootScope'
             data: ticket
         }).then(function successCallback(response) {
             console.log(response.data);
+            $rootScope.HideBusyContainer();
             if (response.data.Status) {
                 $scope.ticketDetails = response.data;
                 // $scope.Message = response.Message;
                 $scope.IsNew = false;
+                $scope.IsFirst = false;
+                $scope.IsSatus = true;
             }
 
         }, function errorCallback(response) {
+            $rootScope.HideBusyContainer();
+            $rootScope.DisplayMessage(response.Message, 'error', "Please Contact Administrator");
+            $scope.IsNew = false;
+            $scope.IsFirst = true;
+            $scope.IsSatus = false;
             console.log(response.data);
         });
     }
+
+    $scope.getAllTicket = function () {
+        $rootScope.ShowBusyContainer("Loading Feedback");
+        $http({
+            method: 'GET',
+            url: $v6urls.processManager + '/processengine/jira/helpdesk/getissues/' + $rootScope.SessionDetails.Domain + '?skip=0&take=50',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then(function successCallback(response) {
+            console.log(response.data.Data.issues);
+            $rootScope.HideBusyContainer();
+            $scope.tasklist = response.data.Data.issues;
+        }, function errorCallback(response) {
+            $rootScope.HideBusyContainer();
+            console.log(response.data);
+        });
+    };
+    $scope.getAllTicket();
 }])
