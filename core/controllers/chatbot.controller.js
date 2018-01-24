7d14c3efb47afbee9f14b5ca69240202b4be3285
Shehan Tis <shehanproductions@ymail.com>
2018-01-24 17:41:23 +0530

    added dependencies

---
+++ core/controllers/chatbot.controller.js
@@ -0,0 +1,3 @@
+app.controller('ChatbotController', ['$scope', '$http', '$v6urls', '$rootScope', 'dataHandler', '$uploader', '$storage', function ($scope, $http, $v6urls, $rootScope, dataHandler, uploader, storage) {
+    
+}])
